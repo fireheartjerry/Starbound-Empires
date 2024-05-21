cb0cd12be433dbe0c23772b2416da603a3efef5a
Jerry Li <54650418+fireheartjerry@users.noreply.github.com>
2024-05-21 13:12:43 -0400

    e

--- Main.java
+++ Main.java
@@ -2,13 +2,12 @@ import hsa.Console;
 
 public class Main {
     static Console c; // HSA Console object
-    static final int FRAME_RATE = 1; // Target frame rate in FPS (frames per second)
-    static final int FRAME_PERIOD = 1000 / FRAME_RATE; // Frame period in milliseconds
 
     public static void main(String[] args) {
         c = new Console(); // Initialize the console
-        long cookies = 0;
-        long increment = 17;
+        
+        // Initalize variables
+        
 
         // Keeping the main thread alive to keep the application running
         while (true) {
