1e8655618164b20807cdd8f1608124a9cd4d3138
Jerry Li <54650418+fireheartjerry@users.noreply.github.com>
2024-05-21 13:34:54 -0400

    Add starting variables

--- Main.java
+++ Main.java
@@ -3,20 +3,22 @@ import hsa.Console;
 public class Main {
     static Console c; // HSA Console object
 
+
+
     public static void main(String[] args) {
         c = new Console(); // Initialize the console
         
-        // Initalize variables
-        
+        // Initialize the game variables for material resources. Energy starts in terajoules, and stellar reserves are in tons.
+        long stellar_reserves, energy;
+        long stellar_reserves_production_rate, energy_production_rate, energy_consumption_rate;
+
+        // Initialize the game variables for the human resources
+        long population, population_growth_rate, soldiers, workers, researchers;
 
         // Keeping the main thread alive to keep the application running
         while (true) {
             // Update the cookies count
-            cookies += increment;
 
-            // Calculate the time taken for the frame update
-            long cookiesPerSecond = increment;
-            c.print("Steel: " + cookies + "\nYou are producing " + cookiesPerSecond + " steel per second\n");
 
             // Calculate the remaining time to sleep to maintain the frame rate
             try {
