7df98148251cbca820f508d71b4e7c8de2527752
Jerry Li <54650418+fireheartjerry@users.noreply.github.com>
2024-05-21 22:03:52 -0400

    Added start screen

--- Main.java
+++ Main.java
@@ -1,29 +1,77 @@
-import hsa.Console;
-import java.awt.*;
+import hsa.*;
+
 import java.awt.Font;
+import java.awt.Color;
+import java.awt.FontFormatException;
+import java.awt.Image;
+
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import java.io.IOException;
+import java.io.FileInputStream;
 
 public class Main {
     static Console c; // HSA Console object
 
-    public static void displayStartingScreen() {
+    public static void displayGraphicalText(String message, Font font, Color col, int x, int y) {
+        c.setFont(font);
+        c.setColor(col);
+        c.drawString(message, x, y);
+    }
+
+    public static void displayBackgroundImage() {
+        Image picture = null;
+        try {
+            picture = ImageIO.read(new File("Assets/stars.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("There was an I/O error when reading the background image file.");
+        }
+        c.drawImage(picture, 0, 0, null);
+    }
+
+    public static void displayStartingScreen(Font customFont) {
         c.clear();
-        c.println("Welcome to");
-        c.println("Press any key to start the game.");
-        
+
+        // Include a nice background image
+        displayBackgroundImage();
+
+        // Write the static messages (they do not require a loop)
+        displayGraphicalText("Welcome To", new Font("Consolas", Font.BOLD, 60), Color.GREEN, 445, 80);
+        displayGraphicalText("Starbound", customFont.deriveFont(150f), Color.CYAN, 100, 250);
+        displayGraphicalText("Empires", customFont.deriveFont(150f), Color.CYAN, 245, 430);
+        displayGraphicalText("Press Any Key to Start the Game", new Font("OCR A Extended", Font.BOLD, 45), Color.YELLOW, 200, 525);
+        displayGraphicalText("Brought to you by Jerry Li and Jerry Chen", new Font("OCR A Extended", Font.PLAIN, 25), Color.GREEN, 325, 600);
+
         c.getChar();
     }
 
     public static void main(String[] args) {
-        c = new Console(30, 135, 18, "Starbound Empires"); // Initialize the console
+        c = new Console(27, 115, 18, "Starbound Empires"); // Initialize the console
 
-        // Set the text color and background color to space themed
+        // Set the text color, background color, and font for the console to match our game theme
         c.setTextBackgroundColor(Color.BLACK);
         c.setTextColor(Color.WHITE);
-        
-        // Set the font to Orbitron
-        Font orbitronFont = new Font("Orbitron", Font.PLAIN, 12);
-        c.setFont(orbitronFont);
-        
+
+        Font customFont = null;
+
+        // Load custom font
+        try {
+            String fontPath = "Assets/gamefont.ttf";
+            File fontFile = new File(fontPath);
+            InputStream fontStream = new FileInputStream(fontFile);
+            customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+            System.err.println("The font file is not in the correct format.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("There was an I/O error when reading the font file.");
+        }
+
         // Initialize the game variables for material resources. Energy starts in terajoules, and stellar reserves are in tons.
         long stellar_reserves, energy;
         long stellar_reserves_production_rate, energy_production_rate, energy_consumption_rate;
@@ -31,7 +79,18 @@ public class Main {
         // Initialize the game variables for the human resources
         long population, population_growth_rate, soldiers, workers, researchers;
 
-        displayStartingScreen();
+        displayStartingScreen(customFont);
+
+        // Remove the starting screen in a cool way using a for loop and a delay
+        for (int i = 0; i < 27; i++) {
+            c.println();
+            try {
+                Thread.sleep(15);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
 
         // Keeping the main thread alive to keep the application running
         while (true) {
