37a1cbe550690e4d88f6130bfda5e5fd7fb0aeb4
Jerry Li <54650418+fireheartjerry@users.noreply.github.com>
2024-05-22 18:07:50 -0400

    Modularize program

--- Main.java
+++ Main.java
@@ -14,20 +14,41 @@ import java.io.IOException;
 import java.io.FileInputStream;
 
 public class Main {
-    static Console c; // HSA Console object
+    static Console c; // HSA Console
 
+    // Game variables
     static long stellar_reserves, energy;
-    static long stellar_reserves_production_rate, energy_production_rate, energy_consumption_rate;
+    static long population, soldiers, workers, doctors, population_capacity;
+    static long seconds_past;
+    static String colony_name;
 
-    static long population, population_growth_rate, soldiers, workers, researchers;
+    // Game rates
+    static double stellar_reserves_production_rate, energy_production_rate, population_growth_rate, energy_consumption_rate;
 
+    /**
+     * Displays graphical text on the screen.
+     *
+     * @param message the text to be displayed
+     * @param font the font to be used for the text
+     * @param col the color of the text
+     * @param x the x-coordinate of the text's position
+     * @param y the y-coordinate of the text's position
+     */
     public static void displayGraphicalText(String message, Font font, Color col, int x, int y) {
         c.setFont(font);
         c.setColor(col);
         c.drawString(message, x, y);
     }
 
-    public static void displayBackgroundImage() {
+    /**
+     * Displays the starting screen of the Starbound Empires game.
+     * 
+     * @param customFont the custom font to be used for displaying text
+     */
+    public static void displayStartingScreen(Font customFont) {
+        c.clear();
+
+        // Include a nice background image
         Image picture = null;
         try {
             picture = ImageIO.read(new File("Assets/stars.jpg"));
@@ -36,13 +57,6 @@ public class Main {
             System.err.println("There was an I/O error when reading the background image file.");
         }
         c.drawImage(picture, 0, 0, null);
-    }
-
-    public static void displayStartingScreen(Font customFont) {
-        c.clear();
-
-        // Include a nice background image
-        displayBackgroundImage();
 
         // Write the static messages (they do not require a loop)
         displayGraphicalText("Welcome To", new Font("Consolas", Font.BOLD, 60), Color.GREEN, 445, 80);
@@ -52,46 +66,6 @@ public class Main {
         displayGraphicalText("Brought to you by Jerry Li and Jerry Chen", new Font("OCR A Extended", Font.PLAIN, 25), Color.GREEN, 315, 600);
 
         c.getChar();
-    }
-
-    
-
-    public static void main(String[] args) {
-        c = new Console(27, 115, 18, "Starbound Empires"); // Initialize the console
-
-        // Set the text color, background color, and font for the console to match our game theme
-        c.setTextBackgroundColor(Color.BLACK);
-        c.setTextColor(Color.WHITE);
-
-        Font customFont = null;
-
-        // Load custom font
-        try {
-            String fontPath = "Assets/gamefont.ttf";
-            File fontFile = new File(fontPath);
-            InputStream fontStream = new FileInputStream(fontFile);
-            customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
-        } catch (FontFormatException e) {
-            e.printStackTrace();
-            System.err.println("The font file is not in the correct format.");
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("There was an I/O error when reading the font file.");
-        }
-
-        // Initialize the game variables for material resources. Energy starts in terajoules, and stellar reserves are in tons.
-        stellar_reserves = 0;
-        energy = 0;
-
-        stellar_reserves_production_rate, energy_production_rate, energy_consumption_rate;
-
-        // Initialize the game variables for the human resources
-        long population, population_growth_rate, soldiers, workers, researchers;
-
-        // Initialize some miscellaneous variables
-        String colony_name; // We initialize it to a string of 13 a's to ensure that the user will always have to change it
-
-        displayStartingScreen(customFont);
 
         // Remove the starting screen in a cool way using a for loop and a delay
         for (int i = 0; i < 27; i++) {
@@ -103,20 +77,12 @@ public class Main {
             }
         }
 
+        c.setCursor(1, 1);
+    }
 
-        do {
-            c.clear();
-            c.print("What is your colony name (max 30 characters)? ");
-            colony_name = c.readLine();
-        } while (colony_name.length() > 30 && colony_name.length() < 1);
-
-        c.clear();
-        // Output the rules to the user
-        displayGraphicalText(colony_name, customFont.deriveFont(50f), Color.CYAN, 10, 45);
-
-        // Output the rules to the user
+    public static void displayRules() {
         displayGraphicalText("Rules:", new Font("OCR A Extended", Font.BOLD, 30), Color.YELLOW, 10, 100);
-        displayGraphicalText("1. You are the leader of a colony in the Starbound Empires universe residing on Earth.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 135);
+        displayGraphicalText("1. You are the leader of a colony in the Starbound Empires universe. You start on Earth.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 135);
         displayGraphicalText("2. You must manage your material resources: stellar reserves and energy.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 170);
         displayGraphicalText("3. You must also manage your human resources: soldiers, workers, and doctors.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 205);
         displayGraphicalText("4. Soldiers will help you conquer new planets.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 240);
@@ -124,18 +90,68 @@ public class Main {
         displayGraphicalText("6. Doctors will increase your population growth.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 310);
         displayGraphicalText("7. The game will become more familiar as you play.", new Font("Consolas", Font.PLAIN, 20), Color.GREEN, 10, 345);
         displayGraphicalText("Press any key to continue...", new Font("OCR A Extended", Font.BOLD, 25), Color.YELLOW, 10, 380);
-
         c.getChar();
         c.clear();
+    }
+
+    public static void intializeGame() {
+        // Set the text color, background color, and font for the console to match our game theme
+        c.setTextBackgroundColor(Color.BLACK);
+        c.setTextColor(Color.WHITE);
+
+        Font customFont = null;
+
+        try {
+            String fontPath = "Assets/gamefont.ttf";
+            File fontFile = new File(fontPath);
+            InputStream fontStream = new FileInputStream(fontFile);
+            customFont = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+            System.err.println("The font file is not in the correct format. Please ensure that it is a .ttf file in the Assets folder.");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("There was an I/O error when reading the font file. Please ensure that it is a .ttf file in the Assets folder.");
+        }
+
+        displayStartingScreen(customFont);
+
+        do {
+            c.print("What is your colony name (max 30 characters)? ");
+            colony_name = c.readLine();
+            c.clear();
+        } while (colony_name.length() > 30 && colony_name.length() < 1);
+
+        displayGraphicalText(colony_name, customFont.deriveFont(50f), Color.CYAN, 10, 45);
+        displayRules();
+        displayGraphicalText(colony_name, customFont.deriveFont(50f), Color.CYAN, 10, 45);
+
+        // We start with 0 stellar reserves and energy
+        stellar_reserves = 0;
+        energy = 0;
+
+        // We start with 1 population, and just 1 worker
+        population = 1;
+        workers = 1;
+        doctors = 0;
+        soldiers = 0;
+
+        seconds_past = 0;
+    }
+
+    public static void main(String[] args) {
+        c = new Console(27, 115, 18, "Starbound Empires"); // Initialize the console
+        intializeGame();
 
         // Keeping the main thread alive to keep the application running
         while (true) {
             // Display the hub
-
+            c.println(seconds_past);
 
             // Calculate the remaining time to sleep to maintain the frame rate
             try {
                 Thread.sleep(1000);
+                seconds_past++;
                 c.setCursor(1, 1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
